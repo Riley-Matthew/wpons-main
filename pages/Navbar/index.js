724cf5c0e359314cc4e772bd358c77e883476b24
Riley-Matthew <mattryllepons@gmail.com>
2025-09-05 22:21:46 +0800

    first commit

---
+++ pages/Navbar/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Image from "next/image";
+const Navbar = () => {
+  const myContact = () => {
+    window.location.href = "/contact";
+  };
+  const myBlog = () => {
+    window.location.href = "/blog";
+  };
+
+  return (
+    <header className="text-gray-600 body-font">
+      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <Image 
+        src="/weh.gif" 
+        alt="Logo" 
+        width={200} 
+        height={100} />
+        
+        <nav className="md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400 flex flex-wrap items-center text-base justify-center">
+            <a className="mr-5 hover:text-gray-900">Bahay</a>
+            <button 
+            onClick={myBlog} 
+            className="mr-5 hover:text-gray-900">Blug </button>
+          <a className="mr-5 hover:text-gray-900">About</a>
+          <button 
+            onClick={myContact} 
+            className="mr-5 hover:text-gray-900">
+            Kontact
+          </button>
+          <a className="mr-5 hover:text-gray-900">Presyo pre</a>
+        </nav>
+
+      </div>
+    </header>
+  );
+};
+
+export default Navbar
+
+// inside of pages folder are reusable components, just ya know  
+// grab the stuff like images, videos , svgs, yabadabadoooa
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Layout from "./layout";
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-red-800 text-3xl font-bold">Pons</h1>
+    </div>
+  );
+
+}
+
